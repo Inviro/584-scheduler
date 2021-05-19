9f6dff8c95abd5e8b795c1850d076bbf33224670
Josh Mejia <jmejia7951@gmail.com>
2021-05-19 10:28:15 -0700

    fixed missing dropdown from merge

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>KhroNotes | Online Scheduling App</title>
-  
+
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
 
@@ -23,63 +23,64 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
-    <!-- import the webpage's javascript file -->
-    <script src="/script.js" defer></script>
-    <script src="https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
+  <!-- import the webpage's javascript file -->
+  <script src="/script.js" defer></script>
+  <script src="https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm {{ $dark_mode ? "navbar-dark bg-dark" : "navbar-light bg-white"}}">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/home') }}">
-                    <!-- {{ str_replace('_', ' ', config('app.name', 'Laravel')) }} -->
-                    <button class="logo_button {{ $dark_mode ? "text-light bg-dark" : ""}}" style="font-size:36px; border: none;">KhroNotes</button>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+  <div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm {{ $dark_mode ? "navbar-dark bg-dark" : "navbar-light bg-white"}}">
+      <div class="container">
+        <a class="navbar-brand" href="{{ url('/home') }}">
+          <!-- {{ str_replace('_', ' ', config('app.name', 'Laravel')) }} -->
+          <button class="logo_button {{ $dark_mode ? "text-light bg-dark" : ""}}" style="font-size:36px; border: none;">KhroNotes</button>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav mr-auto">
 
-                    </ul>
+          </ul>
 
-                    <!-- center of nav -->
-                    <!-- <ul class="navbar-nav mc-auto">
+          <!-- center of nav -->
+          <!-- <ul class="navbar-nav mc-auto">
                         <img class="logo_image" src="https://cdn.glitch.com/8ad63a84-17dc-432f-8c26-21efc67491b6%2FTransparentLogo.png?v=1620239174125" />
                     </ul> -->
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+            @if (Route::has('login'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @endif
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+            @if (Route::has('register'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+            @endif
+            @else
+            <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+              </a>
+
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/home"><i class="fas fa-home"></i>&nbsp;Home</a>
+                <a class="dropdown-item" href="/settings"><i class="fas fa-cog"></i>&nbsp;Settings</a>
+                <a class="dropdown-item" href="{{ route('logout') }}" style="color:red;font-weight:bold;" onclick="event.preventDefault();
+                                                                      document.getElementById('logout-form').submit();">
                   <i class="fas fa-sign-out-alt"></i>&nbsp;Logout
                 </a>
 
