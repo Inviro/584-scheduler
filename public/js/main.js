c653600de0189244fc13c9de4637fdf2782e2182
Josh Mejia <jmejia7951@gmail.com>
2021-05-19 10:42:54 -0700

    moving weather widget javascript into main.js

--- public/js/main.js
+++ public/js/main.js
@@ -90,3 +90,13 @@ function passwordCheck() {
     document.getElementById("saveChanges").setAttribute("disabled", true);
   }
 }
+
+// weather widget 
+!function(d,s,id){
+  var js,fjs=d.getElementsByTagName(s)[0];
+  if(!d.getElementById(id)){
+    js=d.createElement(s);
+    js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+    fjs.parentNode.insertBefore(js,fjs);
+  }
+}(document,'script','weatherwidget-io-js');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -99,9 +99,6 @@
         </script> -->
 
         <a class="weatherwidget-io" href="https://forecast7.com/en/34d28n118d44/san-fernando/?unit=us" data-label_1="NORTHRIDGE, CA" data-label_2="WEATHER" data-days="5" data-theme="weather_one" >NORTHRIDGE, CA WEATHER</a>
-            <script>
-                !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-            </script>
     </div>
 
     <div class="break"></div>
