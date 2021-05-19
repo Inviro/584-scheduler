c76288b049dbdc740ffb3796e663b3855b66c105
Joshua Mejia <jmejia7951@gmail.com>
2021-05-19 02:00:43 -0700

    typo

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\homeController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
